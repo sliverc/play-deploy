2650ce9411b9d9098eb9bc7be6b64361d5582c84
aaronfreeman <aaronwfreeman@gmail.com>
2011-06-03 14:16:40 -0500

    change script to copy Play! to server and check for HAProxy

--- commands.py
+++ commands.py
@@ -20,7 +20,9 @@ def deploy(args, app, env, cmdloader):
 		Server(app, serverName).update()
 
 def precompile(args, app, env, cmdloader):
+	sys.stdout.flush()
 	cmdloader.commands['precompile'].execute(command='precompile', app=app, args=args, env=env, cmdloader=cmdloader)
+	sys.stdout.flush()
 
 def log(msg):
 	print msg
@@ -34,43 +36,61 @@ class Server:
 
 	def update(self):
 		log('updating server ' + self.server)
+		self.checkHaProxy()
 		self.checkPlayInstall()
 		self.setFolderAndPorts()
+		if not self.usingHaProxy:
+			self.stop()
 		self.copyApp()
 		self.start()
-		self.swap()
+		if self.usingHaProxy:
+			self.swap()
+
+	def checkHaProxy(self):
+		self.usingHaProxy = False
+                for line in self.cmd('ps -A').splitlines():
+                        if line.strip().endswith((' haproxy', '\thaproxy')):
+				log('Found HAProxy, will swap in new app when ready')
+                                self.usingHaProxy = True
+				return
 
 	def checkPlayInstall(self):
-		path = self.getPlayRemoteDir()
-		exists = self.cmd("'if test -d " + path  + "; then echo \"exist\"; else echo \"nope\"; fi'").strip()
-		if exists == 'nope':
-			log("Didn't find Play! " + self.env['version'] + " on server, installing to " + path)
-			playName = 'play-' + self.env['version']
-			self.cmd('wget -q -O newPlay http://download.playframework.org/releases/' + playName  + '.zip')
-			self.cmd('unzip -qq newPlay')
-			self.cmd('mkdir -p ' + path)
-			self.cmd('cp -r ' + playName + '/* ' + path)
-			self.cmd('rm -fr ' + playName)
-			self.cmd('rm newPlay')
+		if not self.exists(self.getPlayRemotePath()):
+			log("Didn't find Play! on server in folder " + self.getPlayRemoteDir() + ", installing now")
+			self.copyDir(self.env['basedir'], self.getPlayRemoteDir())
+
+	def exists(self, file):
+		return 'nope' != self.cmd("'if test -f " + file + "; then echo \"exist\"; else echo \"nope\"; fi'").strip()
 
 	def setFolderAndPorts(self):
 		port1 = self.conf('deploy.port1', '9000')
 		port2 = self.conf('deploy.port2', '9001')
-        	base = self.conf('deploy.base', '/opt')
+		base = self.conf('deploy.base', '/opt')
 		folder = os.path.join(base, self.conf('application.name', 'app'))
-		if self.appOneIsRunning(folder + '-1'):
+		folder1 = folder + '-1'
+		folder2 = folder + '-2'
+		if not self.usingHaProxy:
+                        self.newPort = port1
+                        self.oldPort = port1
+                        self.folder = folder
+                        self.oldFolder = folder
+
+		elif self.appOneIsRunning(folder1):
 			self.newPort = port2
 			self.oldPort = port1
-			self.folder = folder + '-2'
-			self.oldFolder = folder + '-1'
+			self.folder = folder2
+			self.oldFolder = folder1
 		else:
 			self.newPort = port1
 			self.oldPort = port2
-			self.folder = folder + '-1'
-			self.oldFolder = folder + '-2'
+			self.folder = folder1
+			self.oldFolder = folder2
 
         def appOneIsRunning(self,folder):
-                pid = self.cmd('cat ' + os.path.join(folder, 'server.pid'))
+		path = os.path.join(folder, 'server.pid')
+		if not self.exists(path):
+			return False
+                pid = self.cmd('cat ' + path)
 		for line in self.cmd('ps -A').splitlines():
 			if line.strip().startswith((pid + ' ', pid + '\t')):
 				return True
@@ -79,7 +99,11 @@ class Server:
 	def copyApp(self):
 		log('copying app to ' + self.folder)
 		self.cmd('rm -fr ' + self.folder)
-		subprocess.call('scp -i ' + self.getPkPath() + ' -qr ' + self.app.path + ' ' + self.getRemoteUser() + '@' + self.server + ':' + self.folder, shell=True)
+		self.copyDir(self.app.path, self.folder)
+
+	def copyDir(self, src, dest):
+		self.cmd('mkdir -p ' + dest)
+		subprocess.call('scp -i ' + self.getPkPath() + ' -qr ' + src + '/* ' + self.getRemoteUser() + '@' + self.server + ':' + dest, shell=True)
 
 	def getPkPath(self):
 		return self.conf('deploy.pk.path', '~/.ssh/id_rsa')
@@ -88,14 +112,14 @@ class Server:
 		return self.conf('deploy.user', 'root')
 
 	def start(self):
-		log('starting new app on')
+		log('starting new app')
 		self.playCmd('start', ' --http.port=' + str(self.newPort)  + ' --%' + self.env["id"] + ' -Dprecompiled=true')
 		self.watchLogFile()
 		log("app started, pinging to make sure it's running properly")
 		self.ping(self.newPort)
 
 	def playCmd(self,cmd,options=''):
-		print self.cmd(self.getPlayRemotePath() + ' ' + cmd + ' ' + self.folder + ' ' + options)
+		log(self.cmd(self.getPlayRemotePath() + ' ' + cmd + ' ' + self.folder + ' ' + options))
 
 	def getPlayRemotePath(self):
 		return os.path.join(self.getPlayRemoteDir(), 'play')
@@ -118,7 +142,7 @@ class Server:
 		contents = self.cmd('cat ' + logFile)
 		if contents.find('Exception') != -1 or timeWaiting > maxWaitTime:
 			self.stop()
-			print 'Error starting app on ' + self.server + '\nsysout contents:\n' + contents
+			log('Error starting app on ' + self.server + '\nsysout contents:\n' + contents)
 			raise Exception('Start Error')
 		if contents.find('~ Listening for HTTP on port ') != -1:
 			return True
@@ -126,7 +150,8 @@ class Server:
 
 
 	def stop(self):
-		print self.cmd(self.getPlayRemotePath() + ' stop ' + self.oldFolder)
+		log('stopping old app')
+		log(self.cmd(self.getPlayRemotePath() + ' stop ' + self.oldFolder))
 
 	def ping(self, port):
 		result = self.cmd('wget -q -O httpTmp localhost:' + port + self.conf('deploy.ping.path', '/'))
@@ -148,10 +173,9 @@ class Server:
 		return value if value else default
 
 	def swap(self):
-		log('everything looks good, now swaping servers')
+		log('everything looks good, now swapping apps')
 		self.sudoCmd('sed -i "s/:' + str(self.oldPort) + '/:' + str(self.newPort) + '/g" /etc/haproxy/haproxy.cfg')
 		log(self.sudoCmd('service haproxy reload'))
 		time.sleep(10)
-		log('stopping old server')
 		self.stop()
 
